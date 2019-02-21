a7fc781183d88738902c421bb54dcb481623ca37
ConnerVerret <cverret@eagles.ewu.edu>
2019-02-20 20:21:08 -0800

    Add seperate .sln for a consumer that can call the web service

---
+++ ZipWebServiceConsumer/ZipConsumer.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ZipConsumer.aspx.cs" Inherits="ZipConsumer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Web Service Consumer</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"
+            integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+            crossorigin="anonymous"></script>
+    <script src="./js/JavaScript.js"></script>
+</head>
+
+<body>
+    <h1>Zip Code Web Service Consumer</h1>
+
+    <form id="form2">
+        <div>
+            Enter Zip: <input type="text" id="zip"/>
+            <input type="button" value="Submit" onclick="CallService()" />
+        </div>
+    </form>
+
+    <p id="resultZip"></p>
+
+</body>
+</html>
---
+++ ZipWebServiceConsumer/js/JavaScript.js
@@ -0,0 +1,29 @@
+﻿function CallService() {
+    var webServiceURL = "http://thatman.connercodes.com/ZipService.asmx?op=GetZip";
+    var soapMessage = `<?xml version="1.0" encoding="utf-8"?><soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope"><soap12:Body><GetZip xmlns="http://thatman.connercodes.com/ZipService"><zip>${ document.getElementById("zip").value }</zip></GetZip></soap12:Body></soap12:Envelope>`;
+
+    $.ajax({
+        url: webServiceURL,
+        type: "POST",
+        dataType: "xml",
+        data: soapMessage,
+        processData: false,
+        contentType: "text/xml; charset=\"utf-8\"",
+        success: OnSuccess,
+        error: OnError
+    });
+}
+
+function OnSuccess(data, status) {
+    var zipResponseTag = data.getElementsByTagName("GetZipResponse").item(0);
+    var zipResultTag = zipResponseTag.getElementsByTagName("GetZipResult").item(0);
+    document.getElementById("resultZip").innerText = zipResultTag.innerHTML;
+}
+
+function OnError(request, status, error) {
+    alert('error');
+}
+
+$(document).ready(function () {
+    jQuery.support.cors = true;
+});
